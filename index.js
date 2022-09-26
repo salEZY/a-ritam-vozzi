8956731d1e781587cba4590bfe642a0fadb8e72d
Aleksandar Bobic <salezybob@gmail.com>
2022-09-26 18:32:55 +0200

    add middlewares

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const db = require("./util/db");
 const routes = require("./routes/index");
+const middlewares = require("./util/middlewares");
 
 const PORT = process.env.PORT || 8090;
 
 const app = express();
 
-app.use(express.json());
+// Middleware
+middlewares(app);
 
+// Server startup
 app.listen(PORT, () => {
   console.log(`VOZZI API started on port: ${PORT}`);
 
+  // Connect to DB
   db();
+
+  // Routes
   routes(app);
 });
