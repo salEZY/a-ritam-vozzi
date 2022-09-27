76ce28add2929f95afdface0373da546733741af
Aleksandar Bobic <salezybob@gmail.com>
2022-09-27 11:12:50 +0200

    slight refactor, add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# VOZZI REST API
+
+## Instructions
+
+- Run git clone https://github.com/salEZY/a-ritam-vozzi.git in the desired folder
+
+- Run npm install in the terminal opened in project root directory
+
+- Create .env in project root directory and copy content from env.example to it
+
+- Start the server with command npm run dev
+
+- Postman collection is located in project root directory
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,4 @@
 module.exports = (app) => {
-  /* if (process.env.NODE_ENV === 'development') {
-      app.use(
-        '/api-docs',
-        swaggerUi.serveFiles(swaggerDocument, {}),
-        swaggerUi.setup(swaggerDocument)
-      );
-    } */
   app.use("/api/user", require("./user-routes"));
   app.use("/api/company", require("./company-routes"));
   app.use("/api/vehicle", require("./vehicle-routes"));
