dc1f4f0c5709ed6cbae5125cebaf7346607e2af9
Aleksandar Bobic <abobic@vozzi.eu>
2022-10-17 13:56:22 +0200

    revert back to ws

--- index.js
+++ index.js
@@ -3,51 +3,28 @@ const express = require("express");
 const db = require("./util/db");
 const routes = require("./routes/index");
 const middlewares = require("./util/middlewares");
-const http = require('http')
+const websocket = require("./util/websocket");
+const http = require("http");
 
-const { Server } = require('socket.io')
-const Location = require('./models/location-model')
-
-
-
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 5070;
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.sendFile(__dirname + "/index.html")
-})
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 // Middleware
 middlewares(app);
 
-const server = http.createServer(app)
-
-const io = new Server(server)
-
-io.on('connection', (socket) => {
-  console.log('A user connected')
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  socket.on('driver-update', async (driverId) => {
-
-    const location = await Location.find({ driver: driverId })
-      .sort({ createdAt: -1 })
-      .limit(1);
-
-    io.emit('driver-update', location)
-  });
-})
+// Websocket connection
+const server = http.createServer(app);
+websocket(server);
 
 server.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`)
-})
+  console.log(`Server started on port: ${PORT}`);
+});
 
-db()
+db();
 
-routes(app)
-
-
+routes(app);
--- util/websocket.js
+++ util/websocket.js
@@ -2,36 +2,29 @@ const ws = require("ws");
 const Location = require("../models/location-model");
 
 module.exports = async (expressApp) => {
-  const websocketServer = new ws.Server({
+  const wss = new ws.Server({
     noServer: true,
-    path: "/websockets",
+    path: "/sockets",
   });
 
   expressApp.on("upgrade", (request, socket, head) => {
-    websocketServer.handleUpgrade(request, socket, head, (websocket) => {
-      websocketServer.emit("connection", websocket, request);
+    wss.handleUpgrade(request, socket, head, (websocket) => {
+      wss.emit("connection", websocket, request);
     });
   });
 
-  websocketServer.on(
-    "connection",
-    async function connection(websocketConnection, connectionRequest) {
-      const [_path, params] = connectionRequest?.url?.split("?");
+  wss.on("connection", async function connection(ws, request) {
+    console.log("Connected");
+    ws.on("message", async (data) => {
+      const id = data.toString();
 
-      // Send drivers last location
-      if (params !== undefined && params?.split("=")[0] === "userId") {
-        const location = await Location.find({ driver: params.split("=")[1] })
-          .sort({ createdAt: -1 })
-          .limit(1);
+      const location = await Location.findOne({ driver: id }).sort({
+        createdAt: -1,
+      });
 
-        websocketConnection.send(
-          `Last location for driver ${location[0].driver?.firstName} ${location[0].driver?.lastName}: ${location[0]}`
-        );
-      } else {
-        websocketConnection.send(`User id is required`);
-      }
-    }
-  );
+      ws.send(JSON.stringify(location));
+    });
+  });
 
-  return websocketServer;
+  return wss;
 };
